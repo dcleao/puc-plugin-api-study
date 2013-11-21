24cc069646eb7c2d6d3b26ee2cb1966c3ac91467
Duarte Cunha Leão <duarte.leao@gmail.com>
2013-11-21 16:43:22 +0000

    Fix the format of the non-main i18n resource.js file.

--- vis-plugin/bar/nls/pt/resources.js
+++ vis-plugin/bar/nls/pt/resources.js
@@ -1,13 +1,9 @@
 define({
-  root: {
-    name: "Barras",
-    description: "Mostra barras horizontais, agrupadas por série, com um comprimento proporcional ao valor de uma medida",
-    visualRoleMulti:     "Mosaico",
-    visualRoleCategory:  "Categoria",
-    visualRoleSeries:    "Cores",
-    visualRoleValue:     "Medida",
-    optionInterpolation: "Dados em falta"
-  }
-
-  pt: true
+  name: "Barras",
+  description: "Mostra barras horizontais, agrupadas por série, com um comprimento proporcional ao valor de uma medida",
+  visualRoleMulti:     "Mosaico",
+  visualRoleCategory:  "Categoria",
+  visualRoleSeries:    "Cores",
+  visualRoleValue:     "Medida",
+  optionInterpolation: "Dados em falta"
 });
